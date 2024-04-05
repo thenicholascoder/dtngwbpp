2eedb2bedca1e7feded42b2f3da052c7cea5904b
thenicholashernandez <thenicholashernandez@gmail.com>
2024-04-05 11:42:29 +0800

    Add restrictions

--- app/controllers/admin/matches_manager_controller.rb
+++ app/controllers/admin/matches_manager_controller.rb
@@ -1,4 +1,7 @@
 class Admin::MatchesManagerController < ApplicationController
+  include LoggedInRestrictions
+  include AdminRestrictions
+
   def index
     @users = User.paginate(page: params[:page]).per_page(10)
   end
